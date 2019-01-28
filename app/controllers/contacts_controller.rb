41164ac58e1675b7fdf7d7e716bff68756c864c2
Ideilson Cisne <ideilson@lit.ifce.edu.br>
2019-01-28 00:48:09 -0300

    Crud part 2

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,22 +4,35 @@ class ContactsController < ApplicationController
   end
 
   def show
+    @contact = Contact.find(params[:id])
   end
 
   def new
     @contact = Contact.new
+    @action = 'create'
   end
 
   def edit
-
+    @contact = Contact.find(params[:id])
+    @action = 'update'
   end
 
   def create
     @contact = Contact.new(contact_params)
+
+    if @contact.save
+      redirect_to contacts_path, alert: "Contato criado com sucesso!"
+    else
+      render :new, alert: "Falha ao criar contato!"
+    end
   end
 
   def update
-
+    if Contact.find(params[:id]).update(contact_params)
+      redirect_to contacts_path, alert: "Contato atualizado com sucesso!"
+    else
+      render :edit, alert: "Falha ao atualizar contato!"
+    end
   end
 
   def destroy
