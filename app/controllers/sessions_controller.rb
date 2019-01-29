1cbc35b6c7ce789eab930b4df312008e894d8ab4
Ideilson Cisne <ideilson@lit.ifce.edu.br>
2019-01-29 02:24:30 -0300

    User authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+    redirect_to user_path(current_user) if user_signed_in?
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in(user)
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Invalid email or password"
+      render "new"
+    end
+  end
+
+  def destroy
+    sign_out
+    flash[:success] = "Successful logout"
+    redirect_to login_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
@@ -7,6 +8,10 @@ Rails.application.routes.draw do
   get "contact-us", to: "static_pages#contact"
   get "contact/:id", to: "contacts#show"
 
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
   resources :contacts
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 end
